4cb2a85ccb81f48995274f684734ae058f69c055
Jason Cope <jason.cope@noaa.gov>
2016-09-11 14:41:16 -0700

    Delete R studio files; Move around SPR examples

--- Shiny_DLMtool/generic_DLM_code.R
+++ Shiny_DLMtool/generic_DLM_code.R
@@ -1,23 +1,30 @@
+#Command line example of using the DLMtool
 #load library
 library(DLMtool)
-#access data objects
+
+#Running TAC estimation with DLMobject
+#access example data objects
 for(i in 1:length(DLMdat))assign(DLMdat[[i]]@Name,DLMdat[[i]])
 avail('DLM_data') 
-Kenya_stock<-new("DLM_data",stock="C:/Program Files/R/R-3.2.3/library/DLMtool/Kenya_stock.csv")
+Ex_dlm.data<-new("DLM_data",stock="C:/Users/Jason.Cope/Documents/GitHub/Data-limited-tools/Shiny_DLMtool/DLM_objects_examples/Example_datafile.csv")
 
-#China rockfish example
-slotNames(China_rockfish) #checks object inputs
-summary(China_rockfish) #Plots summary of removals, abundnace indices and input priors
-Can(China_rockfish)   #Methods that can be used
-Cant(China_rockfish)  #Methods that cannot be use
-Needed(China_rockfish) #What data needed for each "cant" method
+#Example:explore dlm object
+slotNames(Ex_dlm.data) #checks object inputs
+summary(Ex_dlm.data) #Plots summary of removals, abundnace indices and input priors
+Can(Ex_dlm.data)   #Methods that can be used
+Cant(Ex_dlm.data)  #Methods that cannot be use
+Needed(Ex_dlm.data) #What data needed for each "cant" method
 #Calculate and plot catch estimates
-ChinaTAC<-TAC(China_rockfish)
-plot(ChinaTAC)
+Ex_dlm.data.TAC<-TAC(Ex_dlm.data)
+plot(Ex_dlm.data.TAC)
 
-boxplot(t(ChinaTAC@TAC[,,1]),horizontal=TRUE,ylim=c(0,250),axes=F,xlab="Total Allowable Catches",ylab="")
+boxplot(t(Ex_dlm.data.TAC@TAC[,,1]),horizontal=TRUE,ylim=c(0,250),axes=F,xlab="Total Allowable Catches",ylab="")
 box()
 axis(1)
-axis(2,at=c(1:length( ChinaTAC@MPs)),labels= ChinaTAC@MPs,las=2)
+axis(2,at=c(1:length(Ex_dlm.data.TAC@MPs)),labels= Ex_dlm.data.TAC@MPs,las=2)
 #Sensitivity to specified method and its inputs
-China_DCAC_sensi<-Sense(ChinaTAC,"DCAC")
+Example_DCAC_sensi<-Sense(Ex_dlm.data.TAC,"DCAC")
+
+#############################
+
+###### MSE Example ######
