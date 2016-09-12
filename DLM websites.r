4ef4384e3997f825e01d7ee700189c65c47930af
Jason Cope <jason.cope@noaa.gov>
2016-09-12 09:53:11 -0700

    another edit

--- DLM websites.r
+++ DLM websites.r
@@ -3,4 +3,5 @@ DLMtool website: http://www.datalimitedtoolkit.org/
 Jason Cope's GitHub: https://github.com/shcaba
 
 
-Margarita's: http://www.ices.dk/news-and-events/news-archive/news/Pages/IJMS-Editor%E2%80%99s-Choice-%E2%80%93-the-economic-value-of-environmental-data.aspx
+Margarita's anchovy model: http://www.ices.dk/news-and-events/news-archive/news/Pages/IJMS-Editor%E2%80%99s-Choice-%E2%80%93-the-economic-value-of-environmental-data.aspx
+
