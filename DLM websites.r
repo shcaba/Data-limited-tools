97ab9d44018738f0e7c8aa77aecdce199bab70a5
Jason Cope <jason.cope@noaa.gov>
2016-09-15 09:50:38 -0700

    More websites added

--- DLM websites.r
+++ DLM websites.r
@@ -4,6 +4,12 @@ Shiny DLM app weblink: https://shcaba.shinyapps.io/Shiny_DLMtool/
 Jason Cope's GitHub: https://github.com/shcaba
 NOAA Fisheries toolbox: http://nft.nefsc.noaa.gov/index.html
 
+ICES websites
+ICES stock assessment website: https://www.stockassessment.org/login.php
+https://github.com/ices-tools-dev
+https://github.com/ices-tools-prod
+http://ices.dk/marine-data/tools/Pages/stock-assessment-graphs.aspx
+https://sld.ices.dk/manage/index.aspx
 
 Margarita's anchovy model: http://www.ices.dk/news-and-events/news-archive/news/Pages/IJMS-Editor%E2%80%99s-Choice-%E2%80%93-the-economic-value-of-environmental-data.aspx
 
