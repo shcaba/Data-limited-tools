89443e0fdba89b4f5032b78b35fa6bad601f57f9
Jason Cope <jason.cope@noaa.gov>
2016-09-15 02:50:24 -0700

    Add presentations; add weblink; fix Petrale dlm_object

--- DLM websites.r
+++ DLM websites.r
@@ -1,7 +1,9 @@
 Data-limited methods toolbox (Barefoot ecologist): http://barefootecologist.com.au/
 DLMtool website: http://www.datalimitedtoolkit.org/
+Shiny DLM app weblink: https://shcaba.shinyapps.io/Shiny_DLMtool/
 Jason Cope's GitHub: https://github.com/shcaba
 NOAA Fisheries toolbox: http://nft.nefsc.noaa.gov/index.html
+
 
 Margarita's anchovy model: http://www.ices.dk/news-and-events/news-archive/news/Pages/IJMS-Editor%E2%80%99s-Choice-%E2%80%93-the-economic-value-of-environmental-data.aspx
 
